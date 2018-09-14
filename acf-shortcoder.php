7d4f351cd6fdd331cd3e269640192e110681b001
Victor Hugo <vct.bta@gmail.com>
2018-09-13 23:08:17 -0300

    Commit inicial

---
+++ acf-shortcoder.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: ACF Shortcoder
+ * Plugin URI: https://github.com/VictorHugoBatista/acf-shortcoder 
+ * Description: Adiciona shortcode que implementa a função get_field do plugin ACF do wordpress (https://www.advancedcustomfields.com/) .
+ * Author: Victor Hugo Batista
+ * Author URI: https://github.com/VictorHugoBatista 
+ */
+
