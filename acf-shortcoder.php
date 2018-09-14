a12787c026e33c4a6ee53d8dde301acfd8c7ff0d
Victor Hugo <vct.bta@gmail.com>
2018-09-13 23:28:52 -0300

    Adiciona shortcode 'get_field'

--- acf-shortcoder.php
+++ acf-shortcoder.php
@@ -3,8 +3,25 @@
 /**
  * Plugin Name: ACF Shortcoder
  * Plugin URI: https://github.com/VictorHugoBatista/acf-shortcoder 
- * Description: Adiciona shortcode que implementa a função get_field do plugin ACF do wordpress (https://www.advancedcustomfields.com/) .
+ * Description: Adiciona shortcode que implementa a função get_field do plugin ACF do wordpress (https://www.advancedcustomfields.com/).
  * Author: Victor Hugo Batista
  * Author URI: https://github.com/VictorHugoBatista 
  */
+
+add_shortcode('get_field', function($atts) {
+	$a = shortcode_atts([
+		'field' => '',
+		'origin' => '',
+	], $atts);
+	if ('' === $a['field']) {
+		return '';
+	}
+	return gf($a['field'], $a['origin']);
+});
+
+function gf($slug, $value = '') {
+	if (function_exists('get_field')) {
+		return get_field($slug, $value);
+	}
+}
 
